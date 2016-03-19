e38210568d959402230c2c8da30789ec5432a3e6
Steven Magadan <steven.magadan@hotmail.com>
2016-03-18 20:13:04 -0700

    new link

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   'twitter': {
     'consumerKey': 	'ErBfqSML7GNWWakJkqrnpKdqF',
     'consumerSecret': 	'Tt776whQTC82rEMB7KFwzdOGzc9G7AX6FeywRhha2MT2LWpnj1',
-    'callbackURL': 'https://lit-beyond-36103.herokuapp.com/auth/twitter/callback',
+    'callbackURL': 'https://watsonanalysis.herokuapp.com/auth/twitter/callback',
     'profileFields': ['emails', 'displayName']
     // 'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
 
