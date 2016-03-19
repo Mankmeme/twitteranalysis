fdb2ebb640347ac28d2b1cc6551549252f66935e
Steven Magadan <steven.magadan@hotmail.com>
2016-03-18 18:14:50 -0700

    changed auth

--- config/auth.js
+++ config/auth.js
@@ -1,11 +1,12 @@
-var dotenv = require('dotenv').config({silent: true})
+
 
 module.exports = {
   'twitter': {
     'consumerKey': 	'ErBfqSML7GNWWakJkqrnpKdqF',
     'consumerSecret': 	'Tt776whQTC82rEMB7KFwzdOGzc9G7AX6FeywRhha2MT2LWpnj1',
-    'callbackURL': 'https://lit-beyond-36103.herokuapp.com/auth/twitter/callback'
+    'callbackURL': 'https://lit-beyond-36103.herokuapp.com/auth/twitter/callback',
+    'profileFields': ['emails', 'displayName']
     // 'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
-    // 'profileFields': ['emails', 'displayName']
+
   }
 }
