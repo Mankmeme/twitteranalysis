5bc37b604aa1268949b01104341717d3dd87e0f5
steven magadan <steven.magadan@hotmail.com>
2016-12-18 12:41:17 -0800

    zzrot

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(session({
-  secret: "boomboom",
+  secret: "zzrot",
   cookie: {_expires: 6000000}
 }))
 app.use(passport.initialize())
